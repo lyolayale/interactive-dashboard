a76b61ab4a18862f3f0c10b724513ca2c2015a72
Eric McKee <77551058+lyolayale@users.noreply.github.com>
2025-12-28 15:43:11 -0500

    Adds greeting logic

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ setInterval(function () {
   const time = new Date().toLocaleTimeString();
   document.getElementById("clock").textContent = time;
 }, 1000);
+
+// GREETING
+function updateGreeting() {
+  const hour = new Date().getHours();
+  let message = "Hello";
+
+  if (hour < 12) {
+    message = "Good Morning!";
+  } else if (hour < 18) {
+    message = "Good Afternoon!";
+  } else {
+    message = "Good Evening!";
+  }
+
+  document.getElementById("greeting").textContent = message;
+}
+
+updateGreeting();
